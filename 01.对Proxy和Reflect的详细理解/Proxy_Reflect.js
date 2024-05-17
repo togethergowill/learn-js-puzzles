efa8b9e1b7a1d2248baea083ed7068d69d60acb6
HL <HL2972364289@qq.com>
2024-05-17 16:08:37 +0800

    完成Proxy和Reflect的总结

--- 01.对Proxy和Reflect的详细理解/Proxy_Reflect.js
+++ 01.对Proxy和Reflect的详细理解/Proxy_Reflect.js
@@ -1,15 +1,34 @@
 const person = {
-  name: 'ls',
-  age: 18
+  _name: 'ls',
+  age: 18,
+  address: 'China',
+  set name(value) {
+    this.address = value
+    console.log(this)
+  },
+  get name() {
+    console.log(this)
+  }
 }
 
+// Object.defineProperty(person, 'address', {
+//   writable: false
+// })
 
-const personProxy = new Proxy(person,{
-  set(){
-    console.log('it is ok')
+const personProxy = new Proxy(person, {
+  set(target, value, key, receiver) {
+    // 使用Reflect消除静默错误，使用receiver参数改变this指向
+    if (!Reflect.set(target, value, key, receiver)) {
+      throw new Error(`set ${key} 失败  `)
+    } else {
+      console.log("set 成功")
+    }
   }
 })
 
-person.name = 'ly'
+Reflect.get(person, 'name', { name: 'hhhh' })
+
+personProxy.address = '中国'
+console.log(person)
 
 
